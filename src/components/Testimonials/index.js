fbffa66728f9dd01a249940a6e64c363256681c9
Joao Pacheco <jao.pacheco1@gmail.com>
2020-07-18 17:26:55 -0300

    Testimonials ok

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Quotes } from './Quotes';
+import { Testimony } from './Testimony';
 
 import quoteOpen from '../../assets/quote-open.svg';
 import quoteClose from '../../assets/quote-close.svg';
@@ -12,7 +13,10 @@ export const Testimonials = () => {
         <Quotes src={ quoteOpen } />
       </QuotesContainerOpen>
       <Container>
-
+        <Testimony 
+          title="Meu nome é X, tenho 8 anos e fui abusada sexualmente pelo filho da tia que cuida de mim." 
+          text="Quando eu tinha 5 anos, ficava na casa da tia M, ela cuidava de mim enquanto meu papai e minha mamãe iam trabalhar. O filho dela de 23 anos, me chamou para assistirmos um filme enquanto ela estava ausente (não me lembro o que ela foi fazer nesse dia), esse filme tinham pessoas peladas e fazendo coisas que eu não entendia muito bem, e ele, passando a mão em minhas pernas e pedindo para que eu ficasse de calcinha e blusa, pediu que eu repetisse a moça do filme. Eu só consegui contar para a minha mamãe quando fiz 7 anos, pois fiquei com muito medo de não acreditarem em mim." 
+        />
       </Container>
       <QuotesContainerClose>
           <Quotes src={ quoteClose } />
@@ -29,8 +33,8 @@ flex-direction: row;
 `
 
 const Container = styled.div`
-width: 465px;
-height: 296px;
+padding: 24px;
+margin-left: 24px;
 `
 
 const QuotesContainerOpen = styled.div`
