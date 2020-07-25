cd32fb0bb869b6523d82537880432a884a9a9bec
Jon Maciel <joaomaciel.n@gmail.com>
2020-07-25 18:00:16 -0300

    fixing links

--- src/Containers/Main/AttendanceData/index.js
+++ src/Containers/Main/AttendanceData/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import Wrapper from '../../../components/Wrapper';
-import { MainTitle } from '../../../components/MainTitle';
-import { Paragraph } from '../../../components/Paragraph';
-import Button from '../../../components/Button';
-import attendanceImg from '../../../assets/main-attendance.svg';
+import React from "react";
+import styled from "styled-components";
+import Wrapper from "../../../components/Wrapper";
+import { MainTitle } from "../../../components/MainTitle";
+import { Paragraph } from "../../../components/Paragraph";
+import Button from "../../../components/Button";
+import attendanceImg from "../../../assets/main-attendance.svg";
 
 const AttendanceData = () => {
   return (
@@ -13,20 +13,20 @@ const AttendanceData = () => {
         <Container>
           <MainTitle text="DADOS SOBRE O ATENDIMENTO" />
           <Paragraph text="Estamos atuando há 2 (dois) anos, mas só em novembro de 2019 conseguimos legalizar o instituto. Atendemos cerca de 203 (duzentos e três) crianças e mulheres em curso, todas de baixa renda, sendo 168 pessoas em fila de espera." />
-          <Button title="Saiba mais" text={'Saiba mais'} />
+          <Button to="/atendimento" title="Saiba mais" text={"Saiba mais"} />
         </Container>
         <ImageContainer src={attendanceImg} />
       </Wrapper>
     </MainContainer>
-  )
-}
+  );
+};
 
 export default AttendanceData;
 
 const ImageContainer = styled.img`
   width: 650px;
   height: 314px;
-`
+`;
 
 const MainContainer = styled.div`
   height: 430px;
@@ -34,11 +34,11 @@ const MainContainer = styled.div`
   align-itens: center;
   justify-content: center;
   padding: 20px 82px;
-  background: #F0EFFC 0% 0% no-repeat padding-box;
-`
+  background: #f0effc 0% 0% no-repeat padding-box;
+`;
 
 const Container = styled.div`
-height: 100%;
-width: 436px;
-margin: 10px 20px
-`
+  height: 100%;
+  width: 436px;
+  margin: 10px 20px;
+`;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,8 @@
 import React from "react";
-import {
-  ButtonStyled,
-} from "./style";
+import { ButtonStyled } from "./style";
 
-function Button(props) {
-  return (
-    <ButtonStyled>{props.text}</ButtonStyled>
-  );
+function Button({ to, text }) {
+  return <ButtonStyled to={to}>{text}</ButtonStyled>;
 }
 
 export default Button;
