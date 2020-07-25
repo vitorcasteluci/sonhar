864be82686cd6b7b8cd2ca7eb578de9e1845c9bc
luis cazuriaga <luis.cazuriaga@gmail.com>
2020-07-25 10:47:35 -0300

    Help finalizado fontes pendete

--- src/Containers/Help/index.js
+++ src/Containers/Help/index.js
@@ -14,6 +14,28 @@ const Help = () => {
                     <img src={helpBanner} alt="helpBanner"></img>
                 </Picture>
             </Header>
+            <Main>
+                <MainTitle>
+                   QUER NOS AJUDAR?
+                </MainTitle>
+                <MainText>
+               <TextBold>Nossas necessidades são:</TextBold>  
+                <Text>Reforma e mobiliário das salas 
+                para ampliarmos o número de atendimentos 
+                ou um outro imóvel. </Text>
+                <Text>Cestas básicas mensais.</Text>
+                 <Text>Apoio financeiro para o pagamento das contas
+                  mensais de aluguel, luz, telefone, internet e
+                   salário dos funcionários.</Text>
+                    <Text>Lanche diário para as crianças que vem para o reforço escolar.</Text>
+                     <Text>Você pode também pode nos ajudar adotando uma criança pelo valor 
+                     de 30 (Trinta) reais mensais e 
+                    nos seguindo em nossas redes sociais. </Text>
+                    <TextBold>CNPJ: 35.807.142/0001-40 </TextBold>
+                    <TextBold>Banco Stone – 197 </TextBold>
+                    <TextBold>C/c 490953-7 Agência: 0001</TextBold>
+                </MainText>
+            </Main>
             Help
         </div>
     );
@@ -41,5 +63,34 @@ margin-bottom: 126px;
 margin-left: 50px;
 `
 
+const Main = styled.div`
+width: 1366px;
+height : 696px;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const MainTitle = styled.h1`
+color:#7B77FC;
+font-weight:bold;
+`
+
+const MainText = styled.h2`
+font-size: 16px;
+text-align: left;
+margin:80px;
+
+`
+
+const Text = styled.p`
+margin-bottom:40px
+`
+
+const TextBold = styled.p`
+margin-bottom:40px;
+font-weight:bold;
+`
 export default Help
 
