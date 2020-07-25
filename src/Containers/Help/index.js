4b576e5f79abba9ae37f4eff40e7d4262c499b93
Luiz H. S. Mileck <luizhmileck@gmail.com>
2020-07-25 09:33:56 -0300

    img adicionada

--- src/Containers/Help/index.js
+++ src/Containers/Help/index.js
@@ -1,12 +1,34 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+import helpBanner from "../../assets/helpbanner.svg"
 
-
-const Help = () =>{
+const Help = () => {
+    console.log(helpBanner)
     return(
         <div>
+            <Header>
+                <Picture>
+                    <img src={helpBanner} alt="helpBanner"></img>
+                </Picture>
+            </Header>
             Help
         </div>
     );
 }
 
+const Header = styled.div`
+width: 1366px;
+height : 459.25px;
+background-color: #F0EFFC;
+margin: 0 auto;
+`
+
+const Picture = styled.div`
+`
+
+const Title = styled.div`
+
+`
+
 export default Help
+
