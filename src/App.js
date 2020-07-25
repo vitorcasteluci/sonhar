330f1d0e90c756c636119aeee1e79420e190f2b1
Jon Maciel <joaomaciel.n@gmail.com>
2020-07-25 08:18:53 -0300

    Fixing master

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { Router } from './router'
-import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import Main from './pages/Layouts/Main'
-=======
-import Main from './Layouts/Main'
-import { Testimonials } from './components/Testimonials';
-
->>>>>>> feature/Testimonials-quotes
+import React from "react";
+import { Router } from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Main from "./Layouts/Main";
+import { Testimonials } from "./components/Testimonials";
 
 const App = () => {
   return (
-    <Main >
+    <Main>
       <Router />
       <Testimonials />
     </Main>
   );
-}
+};
 
 export default App;
