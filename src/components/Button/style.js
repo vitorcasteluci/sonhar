4934c4e73c3954e2aafabf55c3c63a6fa942e40f
João Maciel Neves Sampaio <joaomaciel.n@gmail.com>
2020-07-25 18:01:23 -0300

    fixing links (#31)

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,10 +1,10 @@
-
-
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const ButtonStyled = styled.button`
+export const ButtonStyled = styled(Link)`
   color: #7b77fc;
   background: #ffffff;
+  padding: 10px;
   border: 1px solid #7b77fc;
   width: 107px;
   height: 38px;
