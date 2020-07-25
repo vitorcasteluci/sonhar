9b6663f24d2a3a3d2ce37074334511a70eb9a325
Luiz H. S. Mileck <luizhmileck@gmail.com>
2020-07-25 09:50:08 -0300

    header done, pending font-family

--- src/Containers/Help/index.js
+++ src/Containers/Help/index.js
@@ -7,6 +7,9 @@ const Help = () => {
     return(
         <div>
             <Header>
+                <Title>
+                    COMO <br /> NOS AJUDAR!
+                </Title>
                 <Picture>
                     <img src={helpBanner} alt="helpBanner"></img>
                 </Picture>
@@ -21,13 +24,21 @@ width: 1366px;
 height : 459.25px;
 background-color: #F0EFFC;
 margin: 0 auto;
+display: flex;
+justify-content: space-between;
+padding: 22px 83px;
+align-items: center;
 `
 
 const Picture = styled.div`
 `
 
-const Title = styled.div`
-
+const Title = styled.h1`
+display: block;
+font-family: Lato;
+font-style: medium;
+margin-bottom: 126px;
+margin-left: 50px;
 `
 
 export default Help
