6bf255aa62daa3802947c807a1eed653d67b8f05
Joao Pacheco <jao.pacheco1@gmail.com>
2020-07-25 18:20:20 -0300

    Fix Statistic

--- src/Containers/About/index.js
+++ src/Containers/About/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import AboutInstitute from './AboutInstitute';
 import MissionValues from './MissionValues';
 import Objective from './Objective';
+import Statistic from './Statistic';
 import Team from './Team';
 
 const About = () => {
-    return (
-        <>
-        <AboutInstitute />
-        <MissionValues />
-        <Objective />
-        <Team />
-        </>
-    )
+  return (
+    <>
+      <AboutInstitute />
+      <MissionValues />
+      <Objective />
+      <Team />
+      <Statistic />
+    </>
+  )
 }
 
 export default About;
