756952de1e34d9990ab938e6b26bc0a70eae4be4
gilairmay <gilairmay@gmail.com>
2020-08-01 10:24:42 -0300

    wip: fixed carousel responsiveness

--- src/Containers/Main/Banner/index.js
+++ src/Containers/Main/Banner/index.js
@@ -5,9 +5,11 @@ import styled from 'styled-components';
 const MainBanner = () => {
     return (
         <Container>
-            <Text>Nosso maior objetivo está em
+            <ImageContainer>
+                <Text>Nosso maior objetivo está em
                 <TextRed> acolher e resgatar </TextRed>
                 a dignidade, a autoestima e sonhos.</Text>
+            </ImageContainer>
         </Container>
     )
 }
@@ -15,12 +17,19 @@ const MainBanner = () => {
 export default MainBanner;
 
 const Container = styled.div`
-    width: 1366px;
+  width: 100%;
+`
+
+const ImageContainer = styled.div`
+    max-width: 1356px;
     height: 619px;
     background-image: url(${ImageMainBanner});
+    background-size: auto;
+    background-repeat: no-repeat;
     position: relative;
     margin: auto;
   `
+
 const Text = styled.div`
     font-family: Lato;
     width: 30%;
