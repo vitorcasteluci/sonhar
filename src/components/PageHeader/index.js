afb75c3046306114e4b51e9e1ff3b172e5a903db
gilairmay <gilairmay@gmail.com>
2020-08-01 17:26:01 -0300

    fix: responsive text and images

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 
-const PageHeader = ({children, src}) => {
+const PageHeader = ({ children, src }) => {
     return (
         <Header>
             <Wrapper>
@@ -27,16 +27,23 @@ padding: 22px 83px;
 `
 const Wrapper = styled.div`
 width:100%;
+flex-wrap: wrap;
 max-width: 1156px;
 display: flex;
-justify-content: space-between;
+justify-content: center;
 align-items: center;
+
 `
 
 const Picture = styled.div`
+flex: 1 1 400px;
+display: flex;
+justify-content: center;
+align-items: center;
 `
 
 const Title = styled.h1`
+flex: 1 1 400px;
 display: block;
 font-family: Lato;
 font-style: medium;
