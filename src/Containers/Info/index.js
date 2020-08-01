d21fde92b6c12b4ebe834a2756f22692f2226927
João Pacheco <64987470+joaoegpacheco@users.noreply.github.com>
2020-08-01 16:52:41 -0300

    Fix/structs (#47)
    * fix struct
    
    * fix structs

--- src/Containers/Info/index.js
+++ src/Containers/Info/index.js
@@ -1,17 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import ArticleContainer from "../../components/ArticleContainer";
 
 const Info = () => {
   return <ArticleContainer />;
 };
-
-const Text = styled.h1`
-  text-align: center;
-  font: Bold 36px/54px Lato;
-  letter-spacing: 1.8px;
-  color: #7b77fc;
-  opacity: 1;
-`;
 
 export default Info;
