ac38fa648a3401c8733d9568a02e5d6e69984759
João Maciel Neves Sampaio <joaomaciel.n@gmail.com>
2020-08-01 16:36:04 -0300

    Fix publication (#46)
    * chore: using lifecycle to force update
    
    * chore: to the top!
    gst

--- src/Containers/Publications/index.js
+++ src/Containers/Publications/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import styled from 'styled-components';
-import { MainTitle } from '../../components/MainTitle';
-import { Paragraph } from '../../components/Paragraph';
-import { SubTitle } from '../../components/SubTitle';
-import Button from '../../components/Button';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../../components/Button";
 
 class Publications extends React.Component {
-
   state = {
     imageUrl: "",
     title: "",
@@ -15,33 +11,38 @@ class Publications extends React.Component {
     publishedAt: "",
     authorName: "",
     nextId: "",
-    previewsId: ""
-  }
+    previewsId: "",
+  };
 
   apiRequest = () => {
-
-    const { id } = this.props.match.params
+    const { id } = this.props.match.params;
     const url = `https://instituto-sonhar-backend.herokuapp.com/public_contents/${id}`;
 
     fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) =>  
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
         // console.log(data)
-          this.setState({ 
-            title: data.content.title, 
-            body: data.content.body, 
-            imageUrl: data.content.image_url, 
-            publishedAt: data.content.published_at, 
-            authorName: data.content.author_name,
-            nextId: data.next_id,
-            previewsId: data.previews_id
-          })
-        )
+        this.setState({
+          title: data.content.title,
+          body: data.content.body,
+          imageUrl: data.content.image_url,
+          publishedAt: data.content.published_at,
+          authorName: data.content.author_name,
+          nextId: data.next_id,
+          previewsId: data.previews_id,
+        })
+      );
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.apiRequest();
+    }
   }
 
   componentDidMount() {
@@ -49,14 +50,17 @@ class Publications extends React.Component {
   }
 
   render() {
-    console.log(this.props.match.params)
     return (
       <MainContainer>
         <Center>
           <Title>{this.state.title}</Title>
         </Center>
         <Center>
-          <img style={{maxWidth: "100%"}} src={this.state.imageUrl} alt={this.state.title} />
+          <img
+            style={{ maxWidth: "100%" }}
+            src={this.state.imageUrl}
+            alt={this.state.title}
+          />
         </Center>
         <Center>
           <Text>{this.state.body}</Text>
@@ -68,61 +72,62 @@ class Publications extends React.Component {
           <Text>Escrito por: {this.state.authorName}</Text>
         </Center>
         <Center>
-          {
-          this.state.previewsId &&
-          <Button text="< Anterior" to={`/publicacoes/${this.state.previewsId}`} />
-          }
-          {
-          this.state.nextId &&
-          <Button text="Próximo >" to={`/publicacoes/${this.state.nextId}`} />
-          }
+          {this.state.previewsId && (
+            <Button
+              text="< Anterior"
+              to={`/publicacoes/${this.state.previewsId}`}
+            />
+          )}
+          {this.state.nextId && (
+            <Button text="Próximo >" to={`/publicacoes/${this.state.nextId}`} />
+          )}
         </Center>
       </MainContainer>
-    )
+    );
   }
 }
 
 export default withRouter(Publications);
 
 const MainContainer = styled.div`
-display: flex;
-justify-content: center;
-flex-direction: column;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Center = styled.div`
-display: flex;
-justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
 
 const Title = styled.h1`
-font-family: Lato;
-font-size: 36px;
-font-weight: bold;
-letter-spacing: 1.8px;
-color: #7B77FC;
-opacity: 1;
-margin-bottom: 40px;
-text-transform: uppercase;
-`
+  font-family: Lato;
+  font-size: 36px;
+  font-weight: bold;
+  letter-spacing: 1.8px;
+  color: #7b77fc;
+  opacity: 1;
+  margin-bottom: 40px;
+  text-transform: uppercase;
+`;
 
 const Text = styled.p`
-font-family: Lato;
-font-size: 16px;
-font-weight: regular;
-letter-spacing: 0px;
-color: #332E2E;
-opacity: 1;
-white-space: pre-line;
-max-width: 1000px;
-padding: 40px;
-`
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: regular;
+  letter-spacing: 0px;
+  color: #332e2e;
+  opacity: 1;
+  white-space: pre-line;
+  max-width: 1000px;
+  padding: 40px;
+`;
 
 const TextPurple = styled.h3`
-font-family: Lato;
-font-size: 16px;
-font-weight: regular;
-letter-spacing: 0px;
-color: #7B77FC;
-opacity: 1;
-`
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: regular;
+  letter-spacing: 0px;
+  color: #7b77fc;
+  opacity: 1;
+`;
